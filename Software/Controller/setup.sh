74dbaf4b3f180ad22aaf9b0d4d822d4c6f6e20fd
Nander Hokwerda <nander@hackershub.io>
2019-05-31 00:59:01 +0200

    Running Blynk server as service and update setup script

--- Software/Controller/setup.sh
+++ Software/Controller/setup.sh
@@ -1,5 +1,8 @@
 apt-get update
-apt-get install build-essential python3-dev python3-smbus git openjdk-8-jdk
+
+apt-get dist-upgrade
+
+apt-get install build-essential python3-dev python3-smbus git openjdk-8-jdk python3-pip
 
 pip3 install -r requirements.txt
 
@@ -13,7 +16,17 @@ cd ..
 
 mkdir /opt/blynkserver
 
-wget "https://github.com/blynkkk/blynk-server/releases/download/v0.41.6/server-0.41.6-java8.jar" -O /opt/blynkserver
+wget "https://github.com/blynkkk/blynk-server/releases/download/v0.41.6/server-0.41.6-java8.jar" -P /opt/blynkserver
 
 mkdir /opt/blynkserver/data
+
+cp blynk_server /etc/init.d
+
+chmod 755 /etc/init.d/blynk_server
+
+update-rc.d blynk_server defaults
+
+/etc/init.d/blynk_server start
+
+echo "Default credentials to log into the local Blynk server are: admin@blynk.cc : admin . To change this log into the web UI on port 9443"
 
